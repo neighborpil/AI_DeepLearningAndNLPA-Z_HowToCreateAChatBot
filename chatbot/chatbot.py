20aa3ea989785d29dc5aef5cbc3cc353f8ab39fa
Neighborpil <neighborpil@naver.com>
2023-06-24 23:54:51 +0900

    save

---
+++ chatbot/chatbot.py
@@ -0,0 +1,14 @@
+import numpy as np
+import tensorflow as tf
+import re
+import time
+
+lines = open('dataset/movie_lines.txt', encoding='utf-8', errors='ignore').read().split('\n')
+conversations = open('dataset/movie_conversations.txt', encoding='utf-8', errors='ignore').read().split('\n')
+
+id2line = {}
+for line in lines:
+    _line = line.split(' +++$+++ ')
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4]
+
